53a02a1a5800343f8ed6505c5110b1bb78caf051
lcdavis13 <3443196+lcdavis13@users.noreply.github.com>
2024-05-14 12:10:09 -1000

    wip

--- train.py
+++ train.py
@@ -41,6 +41,8 @@ else:
 device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 'cpu')
 print(device)
 
+# D: # data samples (rows)
+# N: # species, or columns in data set
 
 # Data processing
 
@@ -49,8 +51,8 @@ inputs = pd.read_csv('data/michel-mata_drosophila_in.csv', index_col=0)
 targets = pd.read_csv('data/michel-mata_drosophila_out.csv', index_col=0)
 
 # Convert to tensors
-inputs = torch.tensor(inputs.values, dtype=torch.float32)
-targets = torch.tensor(targets.values, dtype=torch.float32)
+inputs = torch.tensor(inputs.values, dtype=torch.float32)  # D x N  ( 1410 x 5747 )
+targets = torch.tensor(targets.values, dtype=torch.float32) # D x N  ( 1410 x 5747 )
 
 class CompositionDataset(Dataset):
     def __init__(self, inputs, targets):
@@ -67,10 +69,10 @@ class CompositionDataset(Dataset):
 train_indices, test_indices = train_test_split(list(range(len(inputs))), test_size=0.2, random_state=42)
 
 # Create datasets
-train_dataset = CompositionDataset(inputs[train_indices], targets[train_indices])
-test_dataset = CompositionDataset(inputs[test_indices], targets[test_indices])
+train_dataset = CompositionDataset(inputs[train_indices], targets[train_indices]) # D_train x <test+train> x N  (1128 x 2 x 5747)
+test_dataset = CompositionDataset(inputs[test_indices], targets[test_indices]) # # D_test x <test+train> x N  (282 x 2 x 5747)
 
-batch_size = 32  # You can adjust this based on your specific needs
+batch_size = min(32, len(train_indices))  # B
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=len(test_dataset), shuffle=False)
@@ -78,20 +80,16 @@ test_loader = DataLoader(test_dataset, batch_size=len(test_dataset), shuffle=Fal
 def get_batch(data_loader):
     while True:
         for inputs, targets in data_loader:
-            yield inputs.to(device), targets.to(device)
+            yield inputs.to(device), targets.to(device)  # B x N, B x N  (32 x 5747, 32 x 5747)
             
 train_gen = get_batch(train_loader)
 test_gen = get_batch(test_loader)
 
-# x,y = next(train_gen)
-#
-# print(x.shape)
-# print(y.shape)
-# print(x)
-# print(y)
-
-dimension = inputs.shape[1]
-
+dimension = inputs.shape[1] # N
+print(inputs.shape)
+print(targets.shape)
+print(len(train_indices))
+print(len(test_indices))
 
 #
 # true_y0 = torch.tensor([[2., 0.]]).to(device)
@@ -124,15 +122,46 @@ class ODEFunc(nn.Module):
     def __init__(self):
         super(ODEFunc, self).__init__()
         
-        self.f = nn.Linear(dimension, dimension)
+        self.f = nn.Linear(dimension, dimension) # (N+1) x N
+        #self.ones = torch.ones(dimension, 1) # N x 1
 
-    def forward(self, t, x): # dx/dt = x * (f(x) - x.f(x))
-        fx = self.f(x)
-        xfx = torch.bmm(x.unsqueeze(1), fx.unsqueeze(2)).squeeze(2) # dot product x*fx, but batched
+    def forward(self, t, x):
+        # dx/dt = x * (f(x) - ones . x^T . f(x))
         
-        dxdt = x * (fx - xfx)
+        
+        # x: B x N
+        
+        fx = self.f(x)  # [Bx]Nx(N+1) . (N+1) => [Bx]N
+        
+        # this should be the same as ones*x^T (summing over x), and work for batched data
+        ones_xT = torch.sum(x, dim=-1)  #sum_N([Bx]N) => [B or] 1
+
+        # ones . x^T . f(x) but batched
+        # u = fx * sumx # [B or] 1 x [Bx]N => [Bx]N
+        ones_xT_fx = torch.einsum('...b, ...bn -> ...bn', ones_xT, fx)
+        
+        ones_xT_fx = torch.einsum('...bn, ...bn -> ...bn', x, fx)
+        
+        d = fx - ones_xT_fx # [Bx]N - [Bx]N => [Bx]N
+        
+        dxdt = torch.mul(x, d) # [Bx]N .* [Bx]N => [Bx]N
+        
+
+        #print("forward")
+        #print(x.shape)
+        #print(fx.shape)
+        #print(ones_xT.shape)
+        #print(ones_xT_fx.shape)
+        #print(d.shape)
+        #print(dxdt.shape)
         
         return dxdt
+        
+        # xfx = torch.bmm(x.unsqueeze(1), fx.unsqueeze(2)).squeeze(2) # dot product x*fx, but batched
+        #
+        # dxdt = x * (fx - xfx)
+        #
+        # return dxdt
 
 
 class RunningAverageMeter(object):
@@ -175,6 +204,11 @@ class MinimumMeter(object):
             self.min = val
             self.itr = itr
 
+def loss_bc(p_i,q_i): # Bray-Curtis loss, copied from https://github.com/spxuw/DKI/blob/main/DKI.py
+    # I don't understand how this is BC loss, since that is supposed to have the minimum of the two values in the numerator for each species
+    # but it's the same in the repos for both cNODE papers, so I assume it's correct
+    return torch.sum(torch.abs(p_i - q_i)) / torch.sum(torch.abs(p_i + q_i))  # Should be able to remove the second .abs here?
+
 
 if __name__ == '__main__':
     
@@ -188,7 +222,7 @@ if __name__ == '__main__':
 
     func = ODEFunc().to(device)
     
-    optimizer = optim.RMSprop(func.parameters(), lr=1e-3)
+    optimizer = torch.optim.Adam(func.parameters(), lr=0.01)
     end = time.time()
 
     time_meter = RunningAverageMeter(0.97)
@@ -199,10 +233,10 @@ if __name__ == '__main__':
 
     for itr in range(1, args.niters + 1):
         optimizer.zero_grad()
-        batch_y0, batch_y = next(train_gen)
-        batch_t = torch.tensor([0.0, 1.0]).to(device) # To Do: refactor to remove this time-series input, always want to evaluate at t=1
-        pred_y = odeint(func, batch_y0, batch_t).to(device)
-        loss = torch.mean(torch.abs(pred_y - batch_y))
+        train_y0, train_y = next(train_gen)
+        train_t = torch.tensor([0.0, 1.0]).to(device) # To Do: refactor to remove this time-series input, always want to evaluate at t=1
+        train_pred_y = odeint(func, train_y0, train_t).to(device)
+        loss = loss_bc(train_pred_y, train_y)
         loss.backward()
         optimizer.step()
 
@@ -211,14 +245,14 @@ if __name__ == '__main__':
 
         if itr % args.test_freq == 0:
             with torch.no_grad():
-                true_y0, true_y = next(test_gen)
-                t = torch.tensor([0.0, 1.0]).to(device) # To Do: refactor to remove this time-series input, always want to evaluate at t=1
-                pred_y = odeint(func, true_y0, t)
-                loss = torch.mean(torch.abs(pred_y - true_y))
+                test_y0, test_y = next(test_gen)
+                test_t = torch.tensor([0.0, 1.0]).to(device) # To Do: refactor to remove this time-series input, always want to evaluate at t=1
+                test_pred_y = odeint(func, test_y0, test_t).to(device)
+                loss = loss_bc(test_pred_y, test_y)
                 test_loss_meter.update(loss.item(), itr)
                 print('Iter {:06d} | Train Loss {:.6f} | Test Loss {:.6f} | Min Test Loss {:.6f} @ Iter {:06d}'.format(itr, loss_meter.val, loss.item(), test_loss_meter.min, test_loss_meter.itr))
                 if args.viz:
-                    viz.visualize(true_y, pred_y, func, ii, t, device)
+                    viz.visualize(test_y, test_pred_y, func, ii, test_t, device)
                 ii += 1
 
         end = time.time()
